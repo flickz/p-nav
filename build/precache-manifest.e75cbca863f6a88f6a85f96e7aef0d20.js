b9b3f7a193d4616a10f27126e9b1334a5555b07a
flickz <omoyajowo2015@gmail.com>
2019-01-21 01:51:34 +0100

    Add sample build

---
+++ build/precache-manifest.e75cbca863f6a88f6a85f96e7aef0d20.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "cf71d38dedebeca80717",
+    "url": "/static/js/main.cf71d38d.chunk.js"
+  },
+  {
+    "revision": "80f00736b88aafc14de1",
+    "url": "/static/js/1.80f00736.chunk.js"
+  },
+  {
+    "revision": "cf71d38dedebeca80717",
+    "url": "/static/css/main.b238a2f3.chunk.css"
+  },
+  {
+    "revision": "f1a02151e2417162ae5e79c969c797cb",
+    "url": "/index.html"
+  }
+];
