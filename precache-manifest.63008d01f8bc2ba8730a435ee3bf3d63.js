cf2b79846f8bad4cad1ab382537bea318491e174
flickz <omoyajowo2015@gmail.com>
2019-01-21 02:00:14 +0100

    Updates

---
+++ precache-manifest.63008d01f8bc2ba8730a435ee3bf3d63.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "7d4fb5cce583c098e028",
+    "url": "/p-nav/static/js/runtime~main.7d4fb5cc.js"
+  },
+  {
+    "revision": "cf71d38dedebeca80717",
+    "url": "/p-nav/static/js/main.cf71d38d.chunk.js"
+  },
+  {
+    "revision": "65030156db7d4b99048a",
+    "url": "/p-nav/static/js/1.65030156.chunk.js"
+  },
+  {
+    "revision": "cf71d38dedebeca80717",
+    "url": "/p-nav/static/css/main.b238a2f3.chunk.css"
+  },
+  {
+    "revision": "191076d028d69a78665e951866784b1d",
+    "url": "/p-nav/index.html"
+  }
+];
